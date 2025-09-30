472a63bdfd9d49645d5ebfaf9ae8e16db034005d
youkorr <96953496+youkorr@users.noreply.github.com>
2025-09-30 18:47:38 +0200

    Update tab5_camera.cpp

--- components/tab5_camera/tab5_camera.cpp
+++ components/tab5_camera/tab5_camera.cpp
@@ -86,9 +86,11 @@ bool Tab5Camera::init_camera_() {
 
   memset(&this->camera_config_, 0, sizeof(camera_config_t));
 
-  this->camera_config_.pin_pwdn = -1;
-  this->camera_config_.pin_reset = this->reset_pin_ ? this->reset_pin_->pin() : -1;
-  this->camera_config_.pin_xclk  = this->ext_clock_pin_ ? this->ext_clock_pin_->pin() : 36;
+  
+  this->camera_config_.pin_pwdn  = -1;
+  this->camera_config_.pin_reset = this->reset_pin_ ? this->reset_pin_->pin() : -1;  
+  this->camera_config_.pin_xclk  = this->external_clock_pin_;  // déjà un uint8_t
+
 
 
   this->camera_config_.pin_sccb_sda = 31;
