042b17e68b744895ca6a45753145278840b5a3f0
youkorr <96953496+youkorr@users.noreply.github.com>
2025-09-30 19:06:29 +0200

    Update tab5_camera.cpp

--- components/tab5_camera/tab5_camera.cpp
+++ components/tab5_camera/tab5_camera.cpp
@@ -100,8 +100,8 @@ bool Tab5Camera::init_camera_() {
   
   // Pins CSI pour ESP32-P4 (dédiées)
   this->camera_config_.pin_pwdn = -1;
-  this->camera_config_.pin_reset = this->reset_pin_ ? this->reset_pin_->pin() : -1;
-  this->camera_config_.pin_xclk = this->ext_clock_pin_ ? this->ext_clock_pin_->pin() : 36;
+  this->camera_config_.pin_reset = this->reset_pin_ ? this->reset_pin_->GPIOPin() : -1;
+  this->camera_config_.pin_xclk = this->ext_clock_pin_ ? this->ext_clock_pin_->get_pin() : 36;
   
   // Configuration I2C (SCCB)
   this->camera_config_.pin_sccb_sda = 31;  // GPIO31 CAM_SDA
